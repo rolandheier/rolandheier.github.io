99dc04804d7fd4f89250f7bfe4a88f63eec9668b
rheier <heier.roland@4tress.hu>
2022-08-18 17:14:12 +0200

    átpakolás

--- lib/constants/controllers.dart
+++
@@ -1,5 +0,0 @@
-import 'package:astartapp_web/controllers/menu_controller.dart';
-import 'package:astartapp_web/controllers/navigation_controller.dart';
-
-MenuController menuController = MenuController.instance;
-NavigationController navigationController = NavigationController.instance;
