b03ad7c90d249a844b3c81255439344c4dc1a28e
rheier <heier.roland@4tress.hu>
2022-08-25 16:02:04 +0200

    another try

--- employeetrack/flutter_service_worker.js
+++ employeetrack/flutter_service_worker.js
@@ -17,9 +17,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "f1f61dc83139cf3d6a45dc454c5ec849",
-"/": "f1f61dc83139cf3d6a45dc454c5ec849",
-"main.dart.js": "88ff32b93171728de12139d3a47f7e3b",
+"index.html": "537f14a530f36c0d36b62b7906e939fe",
+"/": "537f14a530f36c0d36b62b7906e939fe",
+"main.dart.js": "ac6a292fdeaa1fd32e7df89977d3cfcb",
 "manifest.json": "09c7ddd85bb1381ce5fa723e883156e7",
 "version.json": "26c7ea229211f77a706fd899ab250c8a"
 };
